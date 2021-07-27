8c34a69985f86762a7a67b6509652e5727568c20
PEDA-ALPHA1 <daep100@gmail.com>
2021-07-27 11:47:53 -0700

    Printf

--- printf-task0.c
+++ printf-task0.c
@@ -1,1 +1,20 @@
 #include "holberton.h"
+#include "stdio.h"
+
+/*
+ * _printf - Print output according to a format
+ *   write output to stdout
+ * @char: string argument
+ * Return: number of char
+ */
+int _printf(const char)
+{
+	char string[] = char;
+	int totalCount;
+	for (i = 0; string[i] != 0; i++)
+	{
+		totalCount += 1;
+	}
+	printf("%d", totalCount);
+	return (totalCount);
+}
